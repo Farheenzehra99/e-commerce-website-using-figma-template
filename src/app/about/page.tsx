bfe373b0586107a8d3d1a8d39b7372ca22814655
Farheen Zehra <farheenzehra481@gmail.com>
2024-12-06 10:21:48 +0500

    hakathon completed

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+
+import About from "../components/About"
+import Views from "../components/Views"
+import Freedelvry from "../components/Free"
+
+
+function AboutPage(){
+    return(
+       <div>
+         <About />
+         <Views />
+         <Freedelvry />
+         {/* about cahnging */}
+       </div>
+    )
+}
+export default AboutPage
